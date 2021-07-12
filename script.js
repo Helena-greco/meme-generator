aa0b78af306348d074b230f023428ca8ca762926
Helena <nenafgreco@hotmail.com>
2021-07-12 20:33:30 -0300

    Requisitos 2 ao 5

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const insertImg = document.getElementById('meme-image');
+const insertMeme = document.getElementById('meme-insert');
 
 function createText() {
   const text = inputText.value;
@@ -7,3 +9,15 @@ function createText() {
 }
 
 inputText.addEventListener('input', createText);
+
+/** Ref: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 */
+function loadFile(event) {
+  const selectImg = event.target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = function onLoad() {
+    insertImg.setAttribute('src', reader.result);
+  };
+  reader.readAsDataURL(selectImg);
+}
+
+insertMeme.addEventListener('change', loadFile);
